8a912c71ffd69b5fadf96c961c9def55d3669d60
yrwq <yrwqid@gmail.com>
2026-02-16 23:46:54 +0100

    parse args, basic account struct

--- src/account.rs
+++ src/account.rs
@@ -1,3 +1,30 @@
+use regex::Regex;
+use anyhow::Result;
+
+use crate::config::Config;
+
 pub struct Account {
-    addr: String,
+    email: String,
+}
+
+impl Account {
+    pub fn add(
+        config: &Config,
+        email: String
+    ) -> Result<()> {
+        config.check_pass_initialized()?;
+
+        let email_regex = Regex::new(r"^.+@.+\.[A-Za-z]+$")?;
+
+        if !email_regex.is_match(&email) {
+            anyhow::bail!("{} is not a valid email address", email);
+        }
+
+        let account = Account {
+            email: email.clone()
+        };
+
+        println!("adding {}", account.email);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use anyhow::{Result};
+use clap::{Parser, Subcommand};
 
 mod config;
 mod account;
 
 use config::Config;
+use account::Account;
+
+
+#[derive(Parser)]
+#[command(name = "mw")]
+#[command(about = "mw: auto-configure email accounts for (neo)mutt", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// add an account
+    Add {
+        /// email address to add
+        email: String,
+    },
+}
+
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let cli = Cli::parse();
     let config = Config::load()?;
 
-    config.check_pass_initialized()?;
-    
+    match cli.command {
+        Commands::Add {
+            email,
+        } => {
+            Account::add(
+                &config,
+                email,
+            )?;
+        }
+    }
+
     Ok(())
 }
