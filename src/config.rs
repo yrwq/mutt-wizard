688023253ee62e35266997cd2efd435026dbc152
yrwq <yrwqid@gmail.com>
2026-02-16 23:32:45 +0100

    check for password store

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use std::{env, path::PathBuf};
+
+use anyhow::{Context, Result};
+
+pub struct Config {
+    password_store: PathBuf
+}
+
+impl Config {
+    pub fn load() -> Result<Self> {
+        let home = env::var("HOME").context("HOME not set")?;
+
+        let password_store = env::var("PASSWORD_STORE_DIR")
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| PathBuf::from(format!("{}/.password-store", home)));
+
+        Ok(Config{
+            password_store
+        })
+    }
+
+    pub fn check_pass_initialized(&self) -> Result<()> {
+        let gpg_id = self.password_store.join(".gpg-id");
+        if !gpg_id.exists() {
+            anyhow::bail!(
+                "run `pass init <yourgpgemail>` to set up a password archive.\n\
+                (if you don't already have a GPG key pair, first run `gpg --full-generate-key`.)"
+            );
+        } else {
+            println!("{:?}", gpg_id);
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use anyhow::{Result};
+
+mod config;
+mod account;
+
+use config::Config;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    let config = Config::load()?;
+
+    config.check_pass_initialized()?;
+    
+    Ok(())
 }
